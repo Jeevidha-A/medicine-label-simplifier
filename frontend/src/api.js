c55166c3fab3378ae1865a73ff94fa3fc8d68f5f
Jeevidha A <jeevidha08bca@gmail.com>
2026-09-11 15:19:24 +0530

    Set production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://medicine-label-simplifier.onrender.com";
 
 export async function analyzeMedicine(file, getToken) {
   const token = await getToken();
